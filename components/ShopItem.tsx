e43a6ba4d671d2029f163225747640628d07a3a7
Your Name <albertzz@bu.edu>
2025-04-28 14:11:23 -0400

    handlebuy sound effect

--- components/ShopItem.tsx
+++ components/ShopItem.tsx
@@ -22,20 +22,31 @@ export default function ShopItem({
                                      onBuy,
                                      disabled,
                                  }: ShopItemProps) {
+    const handleBuy = () => {
+        //does buy logic and plays sound
+        if (disabled) return;
+
+        const buySound = new Audio("/buy.mp3");
+        buySound.currentTime = 0;
+        buySound.play();
+
+        onBuy(); //do the rest of the buy logic
+    };
+
     return (
         <div className="
-        flex justify-between items-center 
+        flex justify-between items-center
         mb-4 p-3 rounded-lg shadow-lg
-        
-        bg-white/10 backdrop-blur-md 
-        border border-white/20 
+
+        bg-white/10 backdrop-blur-md
+        border border-white/20
         ">
             <div>
                 <div className="font-semibold text-white-800">{name}</div>
                 <div className="text-sm text-gray-400">{description}</div>
             </div>
             <button
-                onClick={onBuy}
+                onClick={handleBuy}
                 disabled={disabled}
                 className={`px-4 py-2 rounded-lg font-medium transition duration-200 shadow ${
                     disabled
